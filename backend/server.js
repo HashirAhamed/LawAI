aa393b66c4c77a83eb40bc5f2541b8d5c23f2752
HashirAhamed <hashir.zuhair@gmail.com>
2025-11-06 19:56:17 +0530

    prod x1

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,12 @@
 // server.js
 require("dotenv").config();
 const app = require("./app");
+const cors = require("cors");
+
+app.use(cors({
+  origin: process.env.CLIENT_ORIGIN, 
+  methods: ["GET","POST","PATCH","DELETE","OPTIONS"],
+}));
 
 const PORT = process.env.PORT || 5000;
 
